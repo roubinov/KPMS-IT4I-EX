afb6dbcd3764f9175e86d903995c24f13bd4c529
George Ostrouchov <ostrouchovg@ornl.gov>
2022-03-08 17:48:53 -0500

    8 to 128 cores

--- code/rf_cv_karolina_pbs.sh
+++ code/rf_cv_karolina_pbs.sh
@@ -2,8 +2,8 @@
 #PBS -N rf
 #PBS -l select=1:ncpus=128,walltime=00:50:00
 #PBS -q qexp
-#PBS -e rf_cv_ser.e
-#PBS -o rf_cv_ser.o
+#PBS -e rf_cv_mc.e
+#PBS -o rf_cv_mc.o
 
 cd ~/KPMS-IT4I-EX/code
 pwd
@@ -11,9 +11,14 @@ pwd
 module load R
 echo "loaded R"
 
-time Rscript rf_cv_serial.r
-#time Rscript rf_cv_mc.r 8
-#time Rscript rf_cv_mc.r 16
-#time Rscript rf_cv_mc.r 32
-#time Rscript rf_cv_mc.r 64
-#time Rscript rf_cv_mc.r 128
+#time Rscript rf_cv_serial.r
+echo "8 cores"
+time Rscript rf_cv_mc.r 8
+echo "16 cores"
+time Rscript rf_cv_mc.r 16
+echo "32 cores"
+time Rscript rf_cv_mc.r 32
+echo "64 cores"
+time Rscript rf_cv_mc.r 64
+echo "128 cores"
+time Rscript rf_cv_mc.r 128
