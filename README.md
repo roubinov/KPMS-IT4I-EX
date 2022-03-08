39861a88192330236b3d756b470daea9f04033fe
George Ostrouchov <ostrouchovg@ornl.gov>
2022-03-08 16:48:09 -0500

    add cv

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # KPMS-IT4I-EX
-Companion to KPMS-IT4I with exercises. Separate to avoid lecture pdf replication on IT4I systems.
+Companion to KPMS-IT4I with exercises. It is separate, to avoid lecture pdf replication on IT4I systems.
 
--- code/rf_cv_karolina_pbs.sh
+++ code/rf_cv_karolina_pbs.sh
@@ -11,11 +11,11 @@ pwd
 module load R
 echo "loaded R"
 
-time Rscript rf_serial.r
-time Rscript rf_mc.r 1
-time Rscript rf_mc.r 2
-time Rscript rf_mc.r 8
-time Rscript rf_mc.r 16
-time Rscript rf_mc.r 32
-time Rscript rf_mc.r 64
-time Rscript rf_mc.r 128
+time Rscript rf_cv_serial.r
+time Rscript rf_cv_mc.r 1
+time Rscript rf_cv_mc.r 2
+time Rscript rf_cv_mc.r 8
+time Rscript rf_cv_mc.r 16
+time Rscript rf_cv_mc.r 32
+time Rscript rf_cv_mc.r 64
+time Rscript rf_cv_mc.r 128
