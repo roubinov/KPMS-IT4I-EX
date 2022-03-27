a020a019f31afd9d79666d384cbaa49e5826dab2
Veronika Roubínová <veronika.roubinova@gmail.com>
2022-03-27 13:28:31 +0200

    list into numeric?

--- mnist/mnist_read.R
+++ mnist/mnist_read.R
@@ -17,7 +17,7 @@ test = as.double(h5ts$image)
 dim(test) = c(28*28, 10000)
 test = as.data.frame(t(test))
 test_lab = factor(as.character(h5ts$label))
-plot_test = matrix(test[1,],nrow = 28, ncol = 28)
+plot_test = matrix(as.numeric(test[1,]),nrow = 28, ncol = 28)
 plot_test
 dim(plot_test)
 class(plot_test)
