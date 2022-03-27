49471a51219c84ebec43812055de6e45d3b63111
Veronika Roubínová <veronika.roubinova@gmail.com>
2022-03-27 12:49:40 +0200

    dataframe to matrix

--- mnist/mnist_read.R
+++ mnist/mnist_read.R
@@ -17,6 +17,5 @@ test = as.double(h5ts$image)
 dim(test) = c(28*28, 10000)
 test = as.data.frame(t(test))
 test_lab = factor(as.character(h5ts$label))
-test
 
-image(z=test)
+image(z=data.matrix(test))
