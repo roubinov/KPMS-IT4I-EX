04377d0afdf0db930fc593179c7bb0642caf9d14
George Ostrouchov <ostrouchovg@ornl.gov>
2022-04-07 15:32:09 -0400

    fixed libfabric warning

--- mpi/rf_mpi.sh
+++ mpi/rf_mpi.sh
@@ -11,6 +11,8 @@ pwd
 module load R
 echo "loaded R"
 
+# Fix for warnings from libfabric/1.12 bug
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
+
 time mpirun -np 32 --oversubscribe Rscript rf_mpi.R
 time mpirun -np 64 --oversubscribe Rscript rf_mpi.R
-time mpirun -np 128 --oversubscribe Rscript rf_mpi.R
