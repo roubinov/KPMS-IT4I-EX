bb2721dc99c7aa4314f5d110882438d036e23c22
Veronika Roubínová <veronika.roubinova@gmail.com>
2022-04-16 19:47:03 +0200

    change cores

--- code/EX8.sh
+++ code/EX8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N EX8
-#PBS -l select=2:mpiprocs=128,walltime=00:50:00
+#PBS -l select=2:mpiprocs=64,walltime=00:50:00
 #PBS -q qexp
 #PBS -e ex8.e
 #PBS -o ex8.o
@@ -14,7 +14,7 @@ echo "loaded R"
 # Fix for warnings from libfabric/1.12 bug
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
-time mpirun --map-by ppr:64:node Rscript EX8.R
+time mpirun --map-by ppr:32:node Rscript EX8.R
 
 
 
