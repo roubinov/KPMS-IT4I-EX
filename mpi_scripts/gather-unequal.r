420f7a3d70313243b161717dbb6f7bce204e0017
George Ostrouchov <ostrouchovg@ornl.gov>
2022-04-17 21:49:33 -0400

    added unequal gathers

---
+++ mpi_scripts/gather-unequal.r
@@ -0,0 +1,14 @@
+library( pbdMPI, quiet = TRUE )
+
+comm.set.seed( seed = 1234567, diff = TRUE )
+
+my_rank = comm.rank()
+n <- sample( 1:10, size = my_rank + 1 )
+comm.print(n, all.rank = TRUE)
+
+gt = gather(n)
+
+comm.cat("gathering unequal size objects. lengths =", obj_len, "\n")
+comm.print( unlist( gt ), all.rank = TRUE )
+
+finalize()
